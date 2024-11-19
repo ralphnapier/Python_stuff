8bd4efebc4ba0db511f30addfc29603a2f6dcb06
Ralph Napier <167547084+ralphnapier@users.noreply.github.com>
2024-11-19 15:18:07 -0500

    Create wordBlock.py

---
+++ wordBlock.py
@@ -0,0 +1,13 @@
+name = 'Minceraft'
+space = ''
+beginend = '+' + name + '+'
+
+print(beginend)
+
+for _ in name:
+    space += ' ' 
+    
+for _ in name:
+    print(_ + space + _) 
+  
+print(beginend)
